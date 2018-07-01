5de30d4fcc4367b24ab429650d218e696ae8bb9d
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2018-07-01 20:00:02 +0800

    Initial Commit

---
+++ App.vue
@@ -0,0 +1,33 @@
+<template>
+  <view class="container">
+    <Button :title="message" :on-press="handleBtnPress" />
+  </view>
+</template>
+
+<script>
+export default {
+  data : function(){
+    return {
+      message :  "Vue Native Button"
+    }
+  },
+
+  methods: {
+    handleBtnPress : function() {
+      alert('Btn Press');
+    }
+  }
+}
+</script>
+
+<style>
+.container {
+  background-color: white;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+}
+.text-color-primary {
+  color: blue;
+}
+</style>
